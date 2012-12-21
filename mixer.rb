0ec31b608c1fed0217fce4859cbffeee426dc64a
compwron <wordymusic@gmail.com>
2012-12-20 21:08:59 -0600

    Added more random phrase pieces. The sentences don't make that much sense but it's just brainstorming.

--- mixer.rb
+++ mixer.rb
@@ -1,9 +1,10 @@
 
-verbs = ['build and ask for feedback on', 'facilitate', 'present and ask for feedback on']
+verbs = ['build and ask for feedback on', 'facilitate', 'present and ask for feedback on', 'read a book about', 'attend', 
+	'blog about', 'coordinate']
 
-nouns = ['toy/practice application', 'Hello World app', 'workshop']
+nouns = ['toy/practice application', 'Hello World app', 'workshop', 'conference', 'hackathon']
 
 adjectives = ['Big Data', 'writing for mobile platforms', 'functional programming', 'refactoring legacy code', 
-	'continuous deployment', 'ruby', 'user experience design']
+	'continuous deployment', 'ruby', 'user experience design', 'security']
 
 puts "My goal is to #{verbs[rand(verbs.size)]} a #{nouns[rand(nouns.size)]} showcasing #{adjectives[rand(adjectives.size)]}"
