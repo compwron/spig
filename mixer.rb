ff86aa29087d173c2c0d1795ab18095d88f81da2
Linda Goldstein <lgoldste@thoughtworks.com>
2014-02-23 16:40:34 -0800

    more words

--- mixer.rb
+++ mixer.rb
@@ -2,9 +2,9 @@
 verbs = ['build and ask for feedback on', 'facilitate', 'present and ask for feedback on', 'read a book about', 'attend', 
 	'blog about', 'coordinate']
 
-nouns = ['toy/practice application', 'Hello World app', 'workshop', 'conference', 'hackathon']
+nouns = ['toy/practice application', 'Hello World app', 'workshop', 'conference', 'hackathon', 'lilypond', 'gradle', 'clojure', 'space', 'orbit', '3d printing']
 
 adjectives = ['Big Data', 'writing for mobile platforms', 'functional programming', 'refactoring legacy code', 
-	'continuous deployment', 'ruby', 'user experience design', 'security']
+	'continuous deployment', 'ruby', 'user experience design', 'security', 'secure', 'best practices', 'mobile', 'revolutionary', 'orbital']
 
 puts "My goal is to #{verbs[rand(verbs.size)]} a #{nouns[rand(nouns.size)]} showcasing #{adjectives[rand(adjectives.size)]}"
